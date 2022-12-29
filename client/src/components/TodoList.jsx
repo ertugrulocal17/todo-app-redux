c564233aecb43223ba89da6ebffe27d0a0d7843a
ertugrulocal17 <ertugrulocal17@gmail.com>
2022-12-30 01:55:51 +0300

    add-toggle-and-delete

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,16 +1,29 @@
 import React from "react";
-import { useSelector } from "react-redux";
-
+import { useSelector, useDispatch } from "react-redux";
+import { destroy, toggle } from "../features/todos/todosSlice";
 const TodoList = () => {
   const items = useSelector(state => state.todos.items);
+  const dispatch = useDispatch();
+  const handleDestroy = id => {
+    window.confirm("Are you sure you want to delete this item?") &&
+      dispatch(destroy(id));
+  };
   return (
     <ul className="todo-list">
       {items.map(item => (
         <li className={item.completed ? "completed" : "view"} key={item.id}>
           <div className="view">
-            <input type="checkbox" className="toggle" />
+            <input
+              type="checkbox"
+              className="toggle"
+              checked={item.completed}
+              onChange={() => dispatch(toggle({ id: item.id }))}
+            />
             <label>{item.title}</label>
-            <button className="destroy"></button>
+            <button
+              onClick={() => handleDestroy(item.id)}
+              className="destroy"
+            ></button>
           </div>
         </li>
       ))}
